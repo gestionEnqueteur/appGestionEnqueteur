575b4cc358ea0cd845d2be47ab57cff664d083bf
Rémi <ponche62880@gmail.com>
2023-05-16 15:24:03 +0200

    chore: premier jet du layout navigation
    éléments à finir, c'est juste un premier jet

---
+++ components/navigations/MenuBottomTabs.js
@@ -0,0 +1,49 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from '../../pages/HomeScreen';
+import SettingsScreen from '../../pages/SettingsScreen';
+import OngletsTopTabPlaniSaisi from './OngletsTopTabsPlanifSaisi';
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function MenuBottomTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={OngletsTopTabPlaniSaisi}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
