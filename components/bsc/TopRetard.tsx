06387c9b334d56eb916f869c3cc4613c723b9b2b
Rémi PONCHE <ponche62880@gmail.com>
2023-05-28 01:40:00 +0200

    :poop: Premier jet de la logique TopRetard
    améliorer le formulaire du saisi, pour empecher les caractère non numérique

--- components/bsc/TopRetard.tsx
+++ components/bsc/TopRetard.tsx
@@ -1,15 +1,50 @@
+import { useState } from "react"; 
 import { StyleSheet } from "react-native";
 import { View } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 
-export default function TopRetard() {
+type Props = {
+  labelInput: string;
+  labelButton: string; 
+  time: Date; 
+  onChangeValue: Function; 
+}
+
+export default function TopRetard(props: Props) {
+
+  const [valueInput, setValueInput] = useState(0);
+
+  const handleButton = () => {
+    const currentTime = new Date(); 
+    const retard = new Date(currentTime.getTime() - props.time.getTime()); 
+    setValueInput(retard.getMinutes()); 
+  }
+  
+  function handleInput(newValue: string) {
+
+    try {
+      const minuteRetard = Number(newValue);
+      console.log(Number("")); 
+      // géré le cas de la chaine vie == 0 . 
+      if (!minuteRetard) {
+        return; 
+      } 
+      setValueInput(minuteRetard); 
+      props.onChangeValue(minuteRetard); // on fait remonter l'information au parents 
+    }
+    catch (Error) {
+      setValueInput(0);  
+    }
+
+  }
+
   return (
     <View style={style.container}>
       <View style={style.half}>
-        <TextInput label="Retard au départ" mode="outlined" />
+        <TextInput label="Retard au départ" mode="outlined" value={valueInput.toString()} onChangeText={handleInput} />
       </View>
       <View style={style.half}>
-        <Button mode="contained">Top départ</Button>
+        <Button mode="contained" onPress={handleButton}>Top départ</Button>
       </View>
     </View>
   );
@@ -19,7 +54,7 @@ const style = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center", 
-    gap: 30
+    gap: 20
   },
   half: {
     flexDirection: "column",
