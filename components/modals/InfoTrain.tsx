85dd7630703009f5605869c32ad99bcda2268ab3
Rémi PONCHE <ponche62880@gmail.com>
2023-05-30 01:58:41 +0200

    :lipstick: Mise en place de UI pour modal info train

--- components/modals/InfoTrain.tsx
+++ components/modals/InfoTrain.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { useState } from "react";
+import { View , StyleSheet} from "react-native";
+import { Button, TextInput, Text, SegmentedButtons } from "react-native-paper";
 import styles from "./modalStyle";
 
 export default function InfoTrain() {
+  const [composition, setComposition] = useState("US");
+
   return (
     <View style={styles.modalContainer}>
-      
+      <Text variant="titleLarge">Info train</Text>
+      <Text variant="titleSmall">Composition : </Text>
+      <SegmentedButtons
+        value={composition}
+        onValueChange={setComposition}
+        buttons={[
+          {
+            value: "US",
+            label: "US",
+          },
+          {
+            value: "UM2",
+            label: "UM2",
+          },
+          {
+            value: "UM3",
+            label: "UM3",
+          },
+        ]}
+      />
+      <Text variant="titleSmall">Numéro de matériel : </Text>
+      <TextInput style={style.input} mode="outlined" />
+      <Button mode="contained">Valider</Button>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  input: { 
+    minWidth: 150
+  }
+})
