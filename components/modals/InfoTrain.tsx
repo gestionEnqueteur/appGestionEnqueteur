7c4f2ee66077ad44c71f3e50c82b6fc64bd2c21a
Rémi PONCHE <ponche62880@gmail.com>
2023-05-30 01:41:26 +0200

    :fire: Retire du code pour faire la modal info train

--- components/modals/InfoTrain.tsx
+++ components/modals/InfoTrain.tsx
@@ -6,21 +6,7 @@ import styles from "./modalStyle";
 export default function InfoTrain() {
   return (
     <View style={styles.modalContainer}>
-      <View>
-        <Button mode="contained" style={styles.squareButton}>
-          <Text style={styles.columnTitlePush}>US</Text>
-        </Button>
-        <Button mode="contained" style={styles.squareButton}>
-          <Text style={styles.columnTitlePush}>UM2</Text>
-        </Button>
-        <Button mode="contained" style={styles.squareButton}>
-          <Text style={styles.columnTitlePush}>UM3</Text>
-        </Button>
-      </View>
-      <TextInput />
-      <Button mode="contained" style={styles.button}>
-        <Text style={styles.columnTitlePush}>Valider</Text>
-      </Button>
+      
     </View>
   );
 }
