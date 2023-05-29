3e2b49a4a83ae2bb576d2e7a8a2683a3019d8b8e
Rémi PONCHE <ponche62880@gmail.com>
2023-05-30 01:13:42 +0200

    :fire: Retire du code, pour partir sur des bonnes base

--- components/modals/RetardTrain.tsx
+++ components/modals/RetardTrain.tsx
@@ -1,89 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { View } from "react-native";
 import { Button, TextInput } from "react-native-paper";
-import styles from "./ModalStyle";
+
 
 export default function RetardTrain() {
-  const [retardDepart, setRetardDepart] = useState("");
-  const [retardArrivee, setRetardArrivee] = useState("");
-  const [erreurDepart, setErreurDepart] = useState("");
-  const [erreurArrivee, setErreurArrivee] = useState("");
-
-  const handleDepartChange = (text: string) => {
-    setRetardDepart(text);
-    setErreurDepart("");
-  };
-
-  const handleArriveeChange = (text: string) => {
-    setRetardArrivee(text);
-    setErreurArrivee("");
-  };
-
-  const handleSubmit = () => {
-    // Validation du retard au départ
-    if (!/^\d+$/.test(retardDepart)) {
-      setErreurDepart("Veuillez saisir un retard valide (en minutes).");
-      return;
-    }
-
-    // Validation du retard à l'arrivée
-    if (!/^\d+$/.test(retardArrivee)) {
-      setErreurArrivee("Veuillez saisir un retard valide (en minutes).");
-      return;
-    }
-
-    // Logique de soumission des données
-    console.log("Retard au départ :", retardDepart);
-    console.log("Retard à l'arrivée :", retardArrivee);
-  };
-
-  const dismissKeyboard = () => {
-    Keyboard.dismiss();
-  };
+ 
 
   return (
-    <TouchableWithoutFeedback onPress={dismissKeyboard}>
-      <View style={styles.modalContainer}>
-        <View style={styles.infoRetard}>
-          <View style={styles.saisi}>
-            <TextInput
-              mode="outlined"
-              label="retard au départ"
-              keyboardType="numeric"
-              value={retardDepart}
-              onChangeText={handleDepartChange}
-            />
-            <Text style={styles.errorText}>{erreurDepart}</Text>
-          </View>
-          <View style={styles.topSaisi}>
-            <Button mode="contained" onPress={() => console.log("top départ")}>
-              Top Départ
-            </Button>
-          </View>
-        </View>
-        <View style={styles.infoRetard}>
-          <View style={styles.saisi}>
-            <TextInput
-              mode="outlined"
-              label="retard arrivée"
-              keyboardType="numeric"
-              value={retardArrivee}
-              onChangeText={handleArriveeChange}
-            />
-            <Text style={styles.errorText}>{erreurArrivee}</Text>
-          </View>
-          <View style={styles.topSaisi}>
-            <Button mode="contained" onPress={() => console.log("top arrivée")}>
-              Top Arrivé
-            </Button>
-          </View>
-        </View>
-        <View>
-          <Button mode="contained" onPress={handleSubmit}>
-            Valider
-          </Button>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+    <View>
+
+    </View>
   );
 }
