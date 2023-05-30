801d8d1c8ba2064bf58d28b2e0d898a02af66587
Rémi PONCHE <ponche62880@gmail.com>
2023-05-30 02:43:15 +0200

    :lipstick: Premier jet de l'affichage de liste déroulante

--- components/modals/GareDeDescente.tsx
+++ components/modals/GareDeDescente.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { PaperSelect } from "react-native-paper-select";
 import styles from "./modalStyle";
 
 export default function GareDeDescente() {
+  // juste pour le test
+  const listGare = [
+    { _id: "1", value: "Paris" },
+    { _id: "2", value: "Creil" },
+    { _id: "3", value: "Longueau" },
+  ];
+  const gareSeleted = [{ _id: "2", value: "Creil" }];
   return (
     <View style={styles.modalContainer}>
-      <Text>Gare de descente enqueteur</Text>
-      <TextInput mode="outlined" />
+      <Text variant="titleLarge">Gare de descente enqueteur</Text>
+      <PaperSelect
+        label="gare de descende"
+        arrayList={listGare}
+        selectedArrayList={gareSeleted}
+        multiEnable={false}
+        value="Longueau"
+        onSelection={() => console.log("salut")}
+      />
       <Button mode="contained">Valider</Button>
     </View>
   );
--- components/modals/SupressionTrain.tsx
+++ components/modals/SupressionTrain.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { PaperSelect } from "react-native-paper-select";
 import styles from "./modalStyle";
 
 export default function SupressionTrain() {
+  // juste pour le test
+  const motifSuppression = [
+    { _id: "1", value: "Accident de personne" },
+    { _id: "2", value: "Panne d'un PN" },
+    { _id: "3", value: "Panne Matériel" },
+  ];
+  const motifSuppressionSelected = [{ _id: "2", value: "Panne d'un PN" }];
+
   return (
     <View style={styles.modalContainer}>
-      <Text style={styles.modalContainer}>Motif de suppression du train</Text>
-      <TextInput />
-      <Button mode="contained" style={styles.button}>
-        Valider
-      </Button>
+      <Text variant="titleLarge">Motif de suppresion train</Text>
+      <PaperSelect
+        label="raison d'annulation"
+        arrayList={motifSuppression}
+        selectedArrayList={motifSuppressionSelected}
+        multiEnable={false}
+        value="Panne d'un PN"
+        onSelection={() => console.log("salut")}
+      />
+      <Button mode="contained">Valider</Button>
     </View>
   );
 }
