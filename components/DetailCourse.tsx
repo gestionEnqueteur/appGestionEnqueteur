b66187be4b0b4c3f3f15bbfbb7c4a6f032de982d
Rémi PONCHE <ponche62880@gmail.com>
2023-06-09 14:57:25 +0200

    👔 (bsc): Ajout la navigation pour la saisi de la mesure
    pour les missions de type BSC, redirige vers la saisi, si one clique sur le détails de la course

--- components/DetailCourse.tsx
+++ components/DetailCourse.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, TouchableRipple } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 import ChronoTopDepart from "./ChornoTopDepart";
 import DetailTrajet from "./DetailTrajet";
 import MenuBurger from "./MenuBurger";
 import CardNumeroLine from "./CardNumeroLine";
 import Quotas from "./Quotas";
-
 
 import Course from "../models/Course";
 
@@ -19,8 +19,8 @@ const dateOfTest = new Date();
 export default function DetailCourse(props: Props) {
   const { course } = props;
 
-  const [dateTimeHd, setDateTimeHd] = useState(new Date()); 
-  const [dateTimeHa, setDateTimeHa] = useState(new Date()); 
+  const navigation = useNavigation();
+
 
   const [dateTimeHd, setDateTimeHd] = useState(new Date());
   const [dateTimeHa, setDateTimeHa] = useState(new Date());
@@ -34,33 +34,45 @@ export default function DetailCourse(props: Props) {
     setDateTimeHa(new Date(datetimeArrival));
   }, []);
 
+  const handleGotoAction = () => {
+    switch (course.mission) {
+      case "BSC HDF": 
+        navigation.navigate("SaisiBsc", {course: course});
+      default: 
+        console.log("pas action pour ce type de mission");
+    }
+  }
+
   return (
-    <View style={style.container}>
-      <View></View>
-      <View style={style.lineUp}>
-        <View style={style.detailTime}>
-          <ChronoTopDepart
-            currentDatetime={dateOfTest}
-            datetimeArrival={dateOfTest}
-            datetimeDepart={dateOfTest}
-          />
-          <DetailTrajet
-            departureTime={dateTimeHd}
-            arrivalTime={dateTimeHa}
-            departureCity={course.depart}
-            arrivalCity={course.arrivee}
-          />
+    <TouchableRipple
+      onPress={handleGotoAction}
+    >
+      <View style={style.container}>
+        <View style={style.lineUp}>
+          <View style={style.detailTime}>
+            <ChronoTopDepart
+              currentDatetime={dateOfTest}
+              datetimeArrival={dateOfTest}
+              datetimeDepart={dateOfTest}
+            />
+            <DetailTrajet
+              departureTime={dateTimeHd}
+              arrivalTime={dateTimeHa}
+              departureCity={course.depart}
+              arrivalCity={course.arrivee}
+            />
+          </View>
+          <View>
+            <MenuBurger />
+          </View>
         </View>
-        <View>
-          <MenuBurger />
+        <View style={style.infoLine}>
+          {course.ligne && <CardNumeroLine lineNumber={course.ligne} />}
+          {course.prQuota && <Quotas value={course.prQuota} />}
+          <Text variant="headlineSmall">{course.trainCourse}</Text>
         </View>
       </View>
-      <View style={style.infoLine}>
-        {course.ligne && <CardNumeroLine lineNumber={course.ligne} />}
-        {course.prQuota && <Quotas value={course.prQuota} />}
-        <Text variant="headlineSmall">{course.trainCourse}</Text>
-      </View>
-    </View>
+    </TouchableRipple>
   );
 }
 
