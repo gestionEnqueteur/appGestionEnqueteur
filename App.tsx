356495f5c4694c2d9580ca6611033fa3a26fff03
Rémi PONCHE <ponche62880@gmail.com>
2023-06-13 21:27:48 +0200

    ⚰️ Suppresion de code mort dans App

--- App.tsx
+++ App.tsx
@@ -1,30 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, PaperProvider } from "react-native-paper";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MenuBottomTabs from "./pages/navigations/MenuBottomTabs";
+import { PaperProvider } from "react-native-paper";
 import MyMainStack from "./pages/navigations/StackNavigation";
-
-const Stack = createNativeStackNavigator();
-
-
 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <MyMainStack/>
+        <MyMainStack />
       </NavigationContainer>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
