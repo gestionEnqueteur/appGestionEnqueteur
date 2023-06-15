5195afa54df7f2894a6a20db9982e202b232e0f8
Rémi PONCHE <ponche62880@gmail.com>
2023-06-15 12:50:01 +0200

    🏗️ Wrap App dans le configurationProvider
    Wrap App dans le configurationProvider afin d'acceder au parametre de configuration sans besoin de faire de Drilling Props. juste besoin de se brancher avec le useContext()

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
+import ConfigurationProvider from "./provider/ConfigurationProvider";
 import MyMainStack from "./pages/navigations/StackNavigation";
 
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <MyMainStack />
-      </NavigationContainer>
+      <ConfigurationProvider>
+        <NavigationContainer>
+          <MyMainStack />
+        </NavigationContainer>
+      </ConfigurationProvider>
     </PaperProvider>
   );
 }
